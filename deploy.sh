47cd50f23c0aac37d627cd613fee6de9aa8be3a3
maxigas <maxigas@anargeek.net>
2014-02-16 01:17:32 +0100

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-source Makefile
-$(PELICAN) $(INPUTDIR) -o $(OUTPUTDIR) -s $(PUBLISHCONF) $(PELICANOPTS)
-git add *
-git commit -m deploy.sh
-git push
-ssh biolab@biolab git pull
-echo READY
+make git_upload
